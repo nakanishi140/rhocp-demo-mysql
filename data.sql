a3173b4f1d4ef9ee8d3995bcba6dd00de477a167
yuu <fj7267hc@aa.jp.fujitsu.com>
2020-08-04 15:46:14 +0900

    dd

--- data.sql
+++ data.sql
@@ -4,9 +4,19 @@ create table demo (
  value varchar(255)
 );
 
+create table demo2 (
+ id int,
+ name varchar(255),
+ value varchar(255)
+);
 
-insert into demo value (1,'name01','value01');
+
+
+insert into  value (1,'name01','value01');
 insert into demo value (2,'name02','value02');
 insert into demo value (3,'name03','value03');
 
 
+insert into demo2 value (1,'d2name01','d2value01');
+insert into demo2 value (2,'d2name02','d2value02');
+insert into demo2 value (3,'d2name03','d2value03');
