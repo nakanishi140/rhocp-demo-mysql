37f54daee25810a4ad61fb9ef18aa3417484f0e9
yuu <fj7267hc@aa.jp.fujitsu.com>
2020-08-17 16:29:04 +0900

    ddd

--- data.sql
+++ data.sql
@@ -1,22 +1,31 @@
 create table demo (
  id int,
- name varchar(255),
- value varchar(255)
+ image varchar(255),
+ headline varchar(255),
+ sub_headline varchar(1024),
+ category1  varchar(255),
+ category2  varchar(255),
+ category3  varchar(1024),
 );
 
-create table demo2 (
- id int,
- name varchar(255),
- value varchar(255)
-);
+
+
+insert into demo value (11,'beautiful-2576840_1920.jpg','アウトドアをもっと手軽・快適に','気軽にエレガント　気軽に洗える','summer','title','夏物');
+insert into demo value (12,'casual-1836613_1920.jpg','カジュアルスタイルでグランピング','夏の思い出をUGと共に','summer','title','夏物');
+insert into demo value (13,'fashion-2766734_1920.jpg','そのまま街へでかけよう','夏のスタイルは思いのままに','summer','title','夏物');
+
+insert into demo value (21,'blue-2564660_1920.jpg','アウトドアスタイル','日常でも快適に着られるアウトドアスタイルファッション','summer','contents','夏物');
+insert into demo value (22,'people-2563491_1920.jpg','UGスタイル','UGで自分らしく自由なスタイルを','summer','contents','夏物');
+insert into demo value (23,'hip-hop-1209499_1920.jpg','街角スタイル','街へでかけよう、ファッションを楽しもう','summer','contents','夏物');
+insert into demo value (24,'sunset-1282282_1920.jpg','夏の特別コレクション','より快適に、より心地よいライフスタイルを','summer','contents','夏物');
 
 
 
-insert into demo value (1,'name01','value01');
-insert into demo value (2,'name02','value02');
-insert into demo value (3,'name03','value03');
+insert into demo value (31,'cold-1284029_1920.jpg','冬の空でも快適に','気軽なダウン、UGスタイル','winter','title','冬物');
+insert into demo value (32,'cold-1284028_1920.jpg','子供たちの冬休みは？','雪山でも暖かく、UGスタイル','winter','title','冬物');
 
 
-insert into demo2 value (1,'d2name01','d2value01');
-insert into demo2 value (2,'d2name02','d2value02');
-insert into demo2 value (3,'d2name03','d2value03');
+insert into demo value (41,'fog-3914967_1920.jpg','冬の朝','厳寒の中でも快適で暖かいスタイルファッション','winter','title','冬物');
+insert into demo value (42,'fashion-1063100_1920.jpg','UGスタイル','UGで自分らしく自由なスタイルを','winter','title','冬物');
+insert into demo value (43,'covering-face-1149200_1920.jpg','雪山スタイル','雪山へでかけよう、ファッションを楽しもう','winter','title','冬物');
+
